58ac1f1c308f0ac592897fa4bb04c864811c8578
mdmohiddin <mdmohiddin8000@gamil.com>
2021-09-01 10:33:46 +0600

    commit

--- js/api.js
+++ js/api.js
@@ -15,11 +15,11 @@ function loadPost() {
       fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             // .then(data => console.log(data))
-            .then(data => displayUsers(data));
+            .then(data => displayPost(data));
 }
 // loadPost()
 
-function displayUsers(data) {
+function displayPost(data) {
       console.log(data);
 }
 
@@ -35,13 +35,45 @@ function loadAlbum() {
 function loadUser() {
       fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(MuhammedUser => console.log(MuhammedUser))
+            // .then(user => console.log(user))
+            .then(data => displayUsers(data));
 }
 // loadUser()
+function displayUsers(data) {
+      // console.log(data)
+      const ul = document.getElementById('users')
+            // কাজ-১ (আনঅর্ডার লিস্ট তৈরি করার জন্য)
+      
+      for (const user of data) {
+            console.log(user);
+
+            const li = document.createElement('li');
+            // কাজ-২ (একটি করে লিস্ট বানানোর জন্য)
+
+            // li.innerText = user.name;
+            // dynami ভাবে নামগুলো লিস্ট আকারে দেখাবে
+
+            li.innerText = `Name: ${user.name} E-mail: ${user.email}`
+
+            // কাজ-৩ (লিস্টের মধ্যে নাম গুলো টেক্স আকারে থাকবে বলে)
+
+            ul.appendChild(li);
+            // কাজ-৪ (ইউএল একটি করে চাইল্ড তৈরি করার জন্য)
+      }
+}
+
 
 function loadComments() {
       fetch('https://jsonplaceholder.typicode.com/comments')
             .then(res => res.json())
-            .then(MoHi_Data => console.log(MoHi_Data))
+            // .then(MoHi_Data => console.log(MoHi_Data))
+            .then(data => displayComments(data));
 }
 // loadComments()
+function displayComments(comments) {
+      // console.log(data);
+      for (const comment of comments) {
+            console.log(comment)
+            console.log(comment.email)
+      }
+}
